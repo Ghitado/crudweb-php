098e12a2c8c534786d3c9cd76c96612914e4c158
Ghitado <thiaguinho.020405@gmail.com>
2023-11-10 17:42:58 -0300

    initial commit

---
+++ editar-usuario.php
@@ -0,0 +1,31 @@
+<h1>Novo Usuário</h1>
+
+<?php
+    $sql = "SELECT * FROM usuarios WHERE id=".$_REQUEST["id"];
+    $resp = $conn->query($sql);
+    $row = $resp->fetch_object();
+?>
+
+<form action="?page=salvar" method="POST">
+    <input type="hidden" name="acao" value="editar">
+    <input type="hidden" name="id" value="<?php print $row->id; ?>">
+    <div class="mb-3">
+        <label>Nome</label>
+        <input type="text" name="nome" value="<?php print $row->nome; ?>" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>E-mail</label>
+        <input type="email" name="email" value="<?php print $row->email; ?>" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Senha</label>
+        <input type="password" name="senha" value="<?php print $row->senha; ?>" class="form-control">
+    </div>
+    <div class="mb-3">
+        <label>Data de Nascimento</label>
+        <input type="date" name="data_nasc" value="<?php print $row->data_nasc; ?>" class="form-control">
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+</form>
